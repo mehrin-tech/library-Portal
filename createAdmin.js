e4eff392b97b16b63032580c04d5aec41e0f1524
mehry554 <meharinmehr2@gmail.com>
2026-04-14 14:09:28 +0530

    updated

--- createAdmin.js
+++ createAdmin.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose"
+import dotenv from 'dotenv'
 import Admin from "./models/Admin.js"
 
-mongoose.connect("mongodb://127.0.0.1:27017/libraryDB")
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI)
 
 async function createAdmin() {
 
